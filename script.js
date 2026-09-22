2d7fc270fb6f312c4dec5d96b956087a97d4fa6d
celina29-code <celinaab2911@gmail.com>
2026-09-21 20:51:17 -0600

    ver mas proyectos
    agregamos el boton de js para ver mas opciones

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("el script se esta ejecutando");
+
+const boton = document.querySelector("#ver-mas");
+
+const extra = document.querySelector("#proyectos-extra");
+
+boton.addEventListener("click", function () {
+    extra.classList.toggle("oculto");
+});
+
+
+
